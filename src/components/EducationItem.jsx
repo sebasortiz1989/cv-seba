1ef9b430df84069bf86d80151bf156777bd6f497
Sebastian Ortiz <sebastian.santacoloma@agriculturaverion.com.br>
2025-05-15 17:35:32 -0300

    Updates

--- src/components/EducationItem.jsx
+++
@@ -1,9 +0,0 @@
-const EducationItem = ({ degree, institution, dates, details }) => (
-    <div className="education-item item-card">
-        <h3 className="item-title">{degree} <span className="item-subtitle">- {institution}</span></h3>
-        <p className="item-dates">{dates}</p>
-        {details && <p className="item-description">{details}</p>}
-    </div>
-);
-
-export default EducationItem;
